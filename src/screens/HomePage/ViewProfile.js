cf068eb4c1f0b7575fcadbf76ea5ab17d92ba10e
Declan Loo <188declan@gmail.com>
2024-03-09 22:46:17 +0000

    PUSH THAT CRAP

--- src/screens/HomePage/ViewProfile.js
+++ src/screens/HomePage/ViewProfile.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { getDatabase, ref, get } from "firebase/database";
-
+import { getAuth } from "firebase/auth";
 const userProfileImage = require("../../../assets/userprofile.jpeg");
 
 export const ViewProfile = () => {
+  const [userData, setUserData] = useState({});
+
+  const fetchUserData = async () => {
+    const db = getDatabase();
+    const auth = getAuth();
+    const user = auth.currentUser;
+  
+    if (user) {
+      const userRef = ref(db, `users/${user.uid}`);
+      const snapshot = await get(userRef);
+      if (snapshot.exists()) {
+        setUserData(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    } else {
+      console.log("No user is signed in");
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Image source={userProfileImage} style={styles.profileImage} />
-      <Text style={styles.username}>Name</Text>
+      <Text style={styles.username}>{userData.firstName} {userData.lastName}</Text>
       <View style={styles.infoContainer}>
         <InfoRow
           title="Address:"
-          value="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+          value= {userData.address}
         />
         <InfoRow title="Birthday:" value="0000/20/203" />
-        <InfoRow title="Gender:" value="Apache Helicopter" />
+        <InfoRow title="Gender:" value= {userData.gender} />
         <InfoRow title="Phone No.:" value="Example phone number" />
       </View>
     </ScrollView>
